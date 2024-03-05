8878019f1b859cd2acbb1eb9d5a199c37c0ef665
MohammedYasserBogyMan <smymo94@yahoo.com>
2024-03-05 13:21:42 +0200

    add hive

---
+++ lib/models/note_models.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String desc;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.desc,
+      required this.date,
+      required this.color});
+}
